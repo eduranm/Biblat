4a519dc551db8ca60308bd27de406f411b325592
Herz <achwazer@gmail.com>
2013-10-25 12:51:39 -0500

    Evalute if available full text in search

--- ci/application/views/buscar_index.php
+++ ci/application/views/buscar_index.php
@@ -21,7 +21,7 @@
 		<p class="flag">
 			<em>
 				<?php _printf('<i>%s documentos publicados</i>', $search['total']);?>
-				<?php if(!$textoCompleto): echo anchor("{$paginationURL}/texto-completo", _sprintf(', <b>mostrar "%s" resultados en texto completo</b>', $search['totalCompleto']), 'title="'._sprintf('mostrar %s resultados en texto completo', $search['totalCompleto']).'"'); else:?>
+<?php if(!$textoCompleto && $search['totalCompleto']) > 0): echo ", ".anchor("{$paginationURL}/texto-completo", _sprintf('<b>mostrar "%s" resultados en texto completo</b>', $search['totalCompleto']), 'title="'._sprintf('mostrar %s resultados en texto completo', $search['totalCompleto']).'"'); else:?>
 				en texto completo
 				<?php endif;?>
 			</em>
