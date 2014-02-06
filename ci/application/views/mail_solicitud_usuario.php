cb57133acb97623b94710ef44ab2182f32757899
Herz <achwazer@gmail.com>
2014-02-06 13:08:18 -0600

    Added mail reponse to user in document request

---
+++ ci/application/views/mail_solicitud_usuario.php
@@ -0,0 +1,20 @@
+<?php _e('Estimado/Estimada');?> <?php echo $from;?>:<br/><br/>
+
+<?php _e('Esperando te encuentres bien, te notifico que para la recuperación de este artículo tienes dos opciones:');?><br/><br>
+
+<ol>
+	<li><?php _e('Servicio de fotocopiado  con un costo de 30 centavos cada fotocopia, y tendrías que acudir a nuestras instalaciones a recogerlo.');?></li>
+	<li><?php _e('Envío digital con un costo de  50.00 pesos cada artículo (Si es por este medio se te enviaría una ficha de depósito y hasta el momento que cubras el costo y envíes el comprobante de pago, se te enviaría el documento).');?></li>
+</ol>
+
+<?php _e('Espero tu respuesta a la brevedad posible para darle seguimiento a dicha solicitud.');?><br/><br/>
+
+<?php _e('Atte.');?><br/>
+
+<?php _e('Mtra. Araceli Noguez O.');?><br/>
+
+<?php _e('Correo electrónico:');?> <a target="_blank" href="mailto:anoguez@dgb.unam.mx">anoguez@dgb.unam.mx</a><br/>
+
+<?php _e('Departamento de Información y Servicios Documentales- Subdirección de Servicios de Información Especializada-DGB-UNAM');?><br/>
+
+<?php _e('Teléfono:');?>  56 22 39 60 y 56 22 39 64<br/>
