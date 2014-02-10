def7f61799c7c3e2f9f826fb94d371b4288ee4cc
Herz <achwazer@gmail.com>
2014-02-10 12:46:35 -0600

    Update mail to user request document

--- ci/application/views/revista/mail_solicitud_usuario.php
+++ ci/application/views/revista/mail_solicitud_usuario.php
@@ -1,15 +1,15 @@
 <?php _e('Estimado/Estimada');?> <?php echo $from;?>:<br/><br/>
 
-<?php _e('Esperando te encuentres bien, te notifico que para la recuperación de este artículo tienes dos opciones:');?><br/><br>
+<?php _e('Esperando te encuentres bien, te notifico que, para la recuperación de este artículo, tienes dos opciones:');?><br/><br>
 
 <ol>
-	<li><?php _e('Servicio de fotocopiado  con un costo de 30 centavos cada fotocopia, y tendrías que acudir a nuestras instalaciones a recogerlo.');?></li>
-	<li><?php _e('Envío digital con un costo de  50.00 pesos cada artículo (Si es por este medio se te enviaría una ficha de depósito y hasta el momento que cubras el costo y envíes el comprobante de pago, se te enviaría el documento).');?></li>
+	<li><?php _e('Servicio de fotocopiado, con un costo de 0.30 pesos mexicanos por cada fotocopia, y tendrías que acudir a nuestras instalaciones a recogerlo.');?></li>
+	<li><?php _e('Envío digital con un costo de  50.00 pesos mexicanos (5.00 dólares EUA o 5.00 euros, para solicitudes del extranjero)  por cada artículo.  Si es por este medio te mandaríamos primero una ficha de depósito y, hasta el momento que cubras el costo y nos envíes el comprobante de pago, recibirías el documento en el correo electrónico que nos indiques.');?></li>
 </ol>
 
-<?php _e('Espero tu respuesta a la brevedad posible para darle seguimiento a dicha solicitud.');?><br/><br/>
+<?php _e('Espero tu respuesta a la brevedad posible para darle seguimiento a tu solicitud.');?><br/><br/>
 
-<?php _e('Atte.');?><br/>
+<?php _e('Atentamente:');?><br/>
 
 <?php _e('Mtra. Araceli Noguez O.');?><br/>
 
@@ -17,4 +17,4 @@
 
 <?php _e('Departamento de Información y Servicios Documentales- Subdirección de Servicios de Información Especializada-DGB-UNAM');?><br/>
 
-<?php _e('Teléfono:');?>  56 22 39 60 y 56 22 39 64<br/>
+<?php _e('Teléfonos:');?>  (52-55) 56 22 39 60 y (52-55) 56  22  39  64<br/>
