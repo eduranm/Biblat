816769b9c19e4cd9ca3805159644c61386be7a95
Herz <achwazer@gmail.com>
2014-02-21 22:06:32 -0600

    Update year of copyright

--- ci/application/views/footer.php
+++ ci/application/views/footer.php
@@ -4,7 +4,7 @@
 		<div class="footerRight"></div>
 		<div class="bannerFooter">
 			<div class="copyright">
-				<?php _e('® Derechos reservados. 2009 - 2012. Dirección General de Bibliotecas, Universidad Nacional Autónoma de México (UNAM). Esta página y sus contenidos pueden ser utilizados y reproducidos con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica. De otra forma, requiere permiso previo por escrito de la institución.');?>
+				<?php _printf('® Derechos reservados. 2009 - %d. Dirección General de Bibliotecas, Universidad Nacional Autónoma de México (UNAM). Esta página y sus contenidos pueden ser utilizados y reproducidos con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica. De otra forma, requiere permiso previo por escrito de la institución.', date('Y'));?>
 			</div>
 		</div>
 	</div>
