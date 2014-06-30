c7e63eadfc3dcd4c13e49894d32266a7fcd012f1
Herz <achwazer@gmail.com>
2014-06-30 10:56:34 -0500

    Export in separated files, fix institution secuencue asignation

--- claper_extract.py
+++ claper_extract.py
@@ -2,14 +2,52 @@
 # -*- coding: utf-8 -*-
 import re
 import json
-from collections import OrderedDict
-from pprint import pprint
+from pprint import pprint, pformat
 
 '''Inicializando variables para almacenar las etiquetas del registro y el registro actual'''
 registro = {}
 current = ""
 tags = ['008', '022', '024', '035', '036', '039', '041', '100', '110', '120', '222', '245', '260', '300', '504', '520', '546', '590', '650', '653', '654', '698', '856']
+jsonTags = ('100', '110', '120', '300', '520', '590')
+fieldsArticle = [
+	('sistema', '035'),
+	('revista', '222'),
+	('titulo', '245'),
+	('issn', '022'),
+	('doi', '024'),
+	('paisRevista', '008'),
+	('fechaIngreso', '036'),
+	('idioma', '041'),
+	('anioRevista', '260'),
+	('descripcionBibliografica', '300'),
+	('resumen', '520'),
+	('idiomaResumen', '546'),
+	('documento', '590'),
+	('disciplinaRevista', '698'),
+	('disciplinas', '650'),
+	('palabraClave', '653'),
+	('keyword', '654'),
+	('url', '865')
+]
+fieldsAuthor = [
+	('nombre', 'a'),
+	('email', '6'),
+	('institucionId', 'z')
+]
+fieldsInstitution = [
+	('id', 'z'),
+	('institucion', 'u'),
+	('dependencia', 'v'),
+	('ciudad', 'w'),
+	('pais', 'x')
+]
 lenTags = len(tags)
+'''Abrimos archivos para guradar los resultados'''
+detalles = open('../claperDetalles.txt', 'w')
+fClaPerJSON = open('../claperJSON.txt', 'w')
+fArticle = open('../articuloCLAPER.txt', 'w')
+fAuthor = open('../autorCLAPER.txt', 'w')
+fInstitution = open('../institucionCLAPER.txt', 'w')
 '''Funcion para comparar la afiliación del autor esta duplicada'''
 def duplicate_aff(tag, compare):
 	del compare['z']
@@ -36,32 +74,72 @@ def add_record():
 					del registro['100'][i]['z']
 		del registro['120in100']
 
-	'''Leemos cada etiqueta del registro'''
+	'''Agregamos los registros de documentos'''
 	tagOffset=1
-	for tag in tags:
+	lenFields = len(fieldsArticle)
+	for key, tag in fieldsArticle:
 		if tag in registro:
 			'''Si el tamaño de la etiqueta es 1 asignamos el primer elemento de la lista a la etiqueta'''
-			if len(registro[tag]) == 1 and tag not in ('100', '110', '120', '520', '650', '653', '654'):
+			if len(registro[tag]) > 1 and tag not in ('100', '110', '120', '520', '650', '653', '654', '856'):
+				detalle = {'035':registro['035'], tag:registro[tag]}
+				if registro[tag][0] == registro[tag][1]:
+					for line in registro[tag]:
+						detalles.write('%s %s   L $$a%s\n'%(registro['035'], tag, line))
+						'''detalles.write(pformat(detalle) + "\n")'''
+				'''detalles.write(str(registro['035']) + " " + str(tag) + "\n")'''
+			if len(registro[tag]) == 1 and tag not in ('100', '110', '120', '520', '650', '653', '654', '856'):
 				registro[tag] = registro[tag].pop()
 			if type(registro[tag]) in [type({}), type([])]:
-				csv += "'"+ re.sub(r"\\\\", r"\\\\\\", re.sub(r"'", r"\\'", json.dumps(registro[tag], ensure_ascii=True, sort_keys=True))) + "'"
+				csv += "'"+ re.sub(r"'", r"\\'", re.sub(r"\\", r"\\\\", json.dumps(registro[tag], ensure_ascii=True, sort_keys=True))) + "'"
 			else:
 				csv += "'"+ re.sub(r"'", "\\'", registro[tag]) + "'"
 		else:
 			csv += "NULL"
-		if tagOffset < lenTags:
+		if tagOffset < lenFields:
 			csv += ","
 		tagOffset += 1
 	'''Transformamos el diccionario en JSON'''
 	'''jsonResult = re.sub(r"\\\\\\", "\\\\", json.dumps(registro, ensure_ascii=False, sort_keys=True))'''
 	'''Almacenamos la cadena JSON en el archivo'''
-	if registro['035'] == "PERA01000279167":
-		jsonResult = re.sub(r"\\\\", r"\\\\\\", json.dumps(registro['120'], ensure_ascii=False, sort_keys=True))
-		pprint(jsonResult)
+	if registro['035'] == "CLA01000006501A":
 		pprint(registro)
 		raw_input("Press Enter to continue...")
-	with open('../claperJSON.txt', 'a') as fileJSON:
-		fileJSON.write(csv + "\n")
+	fArticle.write(csv + "\n")
+	csv = None
+
+	'''Agregamos los registros de autores'''
+	lenFields = len(fieldsAuthor)
+	if '100' in registro:
+		for counter,autor  in enumerate(registro['100']):
+			csv =  "'%s',"%registro['035']
+			csv += "'%d',"%(counter+1)
+			tagOffset=1
+			for field, tag in fieldsAuthor:
+				if tag in autor:
+					csv += "'"+ re.sub(r"^\(([0-9]+?)\)$", r"\1", re.sub(r"'", "\\'", autor[tag])) + "'"
+				else:
+					csv += "NULL"
+				if tagOffset < lenFields:
+					csv += ","
+				tagOffset += 1
+			fAuthor.write(csv + "\n")
+
+	'''Agregamos los registros de instituciones'''
+	lenFields = len(fieldsInstitution)
+	if '120' in registro:
+		for institucion  in registro['120']:
+			csv = "'%s',"%registro['035']
+			tagOffset=1
+			for field, tag in fieldsInstitution:
+				if tag in institucion:
+					csv += "'"+ re.sub(r"^\(([0-9]+?)\)$", r"\1", re.sub(r"'", "\\'", institucion[tag])) + "'"
+				else:
+					csv += "NULL"
+				if tagOffset < lenFields:
+					csv += ","
+				tagOffset += 1
+			fInstitution.write(csv + "\n")
+
 	registro = {}
 
 '''Abrimos el archivo con que contiene el resultado de p_print_03 de aleph'''
@@ -128,7 +206,7 @@ def parse_database(database):
 
 				for etiqueta, subtag in subtags.iteritems():
 					'''Si el tamanio del diccionario es de 1 almacenamos el valor del elemenro en la etiqueta'''
-					if len(subtag) == 1 and etiqueta not in ('100', '110', '120', '520'):
+					if len(subtag) == 1 and etiqueta not in jsonTags:
 						subtag = subtag.itervalues().next()
 					'''Si la etiqueta no esta en el diccionario del registro inicializamos una lista'''
 					if etiqueta not in registro and etiqueta != "520":
@@ -145,16 +223,25 @@ def parse_database(database):
 
 '''Funcion para agregar los registros de clase y periodica'''
 def add_claper():
-	'''Abrimos archivo para almacenar el resultado del proceso'''
-	with open('../claperJSON.txt', 'w') as file:
-		tagOffset=1
-		for tag in tags:
-			file.write("'" + tag + "'")
-			if tagOffset < lenTags:
-				file.write(",")
-			else:
-				file.write("\n")
-			tagOffset += 1
+	'''Escribimos encabezados de los archivos'''
+	'''tagOffset=1
+	for tag in tags:
+		fClaPerJSON.write("'" + tag + "'")
+		if tagOffset < lenTags:
+			fClaPerJSON.write(",")
+		else:
+			fClaPerJSON.write("\n")
+		tagOffset += 1
+
+	fieldOffset=1
+	lenFields = len(fieldsArticle)
+	for field in fieldsArticle:
+		fArticle.write("'" + field + "'")
+		if fieldOffset < lenFields:
+			fArticle.write(",")
+		else:
+			fArticle.write("\n")
+		fieldOffset += 1'''
 	parse_database('../clase_valid.txt')
 	parse_database('../periodica_valid.txt')
 
