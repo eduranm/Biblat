94459071aa93311b515d885826e9581c61b0437c
Herz <achwazer@gmail.com>
2014-07-04 10:29:46 -0500

    update performance script

--- Performance.sql
+++ Performance.sql
@@ -87,28 +87,10 @@ UPDATE articulo SET e_546=NULL WHERE e_546='';
 UPDATE articulo SET e_856u=NULL WHERE e_856u='';
 UPDATE articulo SET e_222=NULL WHERE e_222='';
 UPDATE articulo SET id_disciplina=NULL WHERE id_disciplina<1;
-UPDATE articulo SET e_222='Bioquimia' WHERE e_222='Bioquimía';
-UPDATE articulo SET e_222='Anales del Instituto de Biología. UNAM. Serie zoología' WHERE e_222='Anales del Instituto de Biología. UNAM. Serie Zoología';
-UPDATE articulo SET e_222='Civitas. Revista de ciencias sociais' WHERE e_222='Civitas. Revista de ciencias sociais,';
-UPDATE articulo SET e_222='Cognitio. Revista de filosofia' WHERE e_222='Cognitio. Revista de Filosofia';
-UPDATE articulo SET e_222='Conjuntura economica (Rio de Janeiro)' WHERE e_222='Conjuntura económica (Rio de Janeiro)';
-UPDATE articulo SET e_222='Enseñanza e investigación en psicología' WHERE e_222='Enseñanza e investigación en Psicología';
-UPDATE articulo SET e_222='Estudios - Instituto Tecnológico Autónomo de México' WHERE e_222='Estudios - Instituto Tecnológico Autónomo de México,';
-UPDATE articulo SET e_222='Estudos de psicologia (Natal)' WHERE e_222='Estudos de Psicologia (Natal)';
-UPDATE articulo SET e_222='GénEros' WHERE e_222='Géneros';
-UPDATE articulo SET e_222='Journal of Eastern Caribbean studies' WHERE e_222='Journal of eastern caribbean studies';
-UPDATE articulo SET e_222='Novos estudos - CEBRAP' WHERE e_222='Novos Estudos - CEBRAP';
 UPDATE articulo SET e_222='Psicologia: reflexao e critica' WHERE e_222 in ('Psicologia: Reflexao e critica', 'Psicologia: Reflexão e Crítica');
-UPDATE articulo SET e_222='Revista brasileira de educacao' WHERE e_222='Revista Brasileira de Educação';
-UPDATE articulo SET e_222='Revista brasileira de reumatologia' WHERE e_222='Revista Brasileira de Reumatologia';
 UPDATE articulo SET e_222='Revista de economía política' WHERE e_222='Revista de economia politica';
-UPDATE articulo SET e_222='Revista de extensión cultural - Universidad Nacional de Colombia' WHERE e_222='Revista de Extensión Cultural - Universidad Nacional de Colombia';
 UPDATE articulo SET e_222='Revista de la Facultad de Farmacia de la Universidad de los Andes' WHERE e_222='Revista de la Facultad de Farmacia de la Universidad de Los Andes';
-UPDATE articulo SET e_222='Revista médica del Instituto Mexicano del Seguro Social' WHERE e_222='Revista médica del Instituto Mexicano del Seguro Social,';
-UPDATE articulo SET e_222='Revista mexicana de orientación educativa' WHERE e_222='Revista Mexicana de Orientación Educativa';
-UPDATE articulo SET e_222='Sao Paulo em perspectiva' WHERE e_222='São Paulo em Perspectiva';
 UPDATE articulo SET e_222='Sociedade e estado' WHERE e_222='Sociedade e Estado';
-UPDATE articulo SET e_222='Symposium - Universidade Catolica de Pernambuco' WHERE e_222='Symposium - Universidade Católica de Pernambuco';
 UPDATE articulo SET e_222='Utopía y praxis latinoamericana' WHERE e_222='Utopía y Praxis Latinoamericana';
 
 UPDATE articulo SET e_008='Colombia' WHERE e_222='Academia. Revista latinoamericana de administración';
@@ -154,10 +136,8 @@ UPDATE articulo SET e_008='México' WHERE e_008='Mexico' OR e_008='Mèxico';
 ALTER TABLE articulo ADD COLUMN "revistaSlug" varchar;
 ALTER TABLE articulo ADD COLUMN "e_698" varchar;
 UPDATE articulo SET "revistaSlug"=slug(e_222);
-UPDATE articulo a SET id_disciplina=r.id_disciplina 
-FROM rev_disciplinas r WHERE a."revistaSlug"=r."revistaSlug";
 UPDATE articulo a SET id_disciplina=d.id_disciplina 
-FROM disciplinas d WHERE slug(a.e_698)=d.slug;
+  FROM disciplinas d WHERE slug(a.e_698)=d.slug;
 /*Indices para optimizar las consultas*/
 CREATE INDEX "articuloTextoCompleto_idx" ON articulo(e_856u);	
 CREATE INDEX "articuloAlfabetico_idx" ON articulo(substring(LOWER(e_222), 1, 1));
