0b8bdf1925197780a9dfc0cf5ed5bfa794fcbc9f
Herz <achwazer@gmail.com>
2014-11-24 10:17:51 -0600

    Adjust image size

--- ci/application/views/main/materiales_difusion.php
+++ ci/application/views/main/materiales_difusion.php
@@ -1,5 +1,33 @@
 <div class="page_title">
     <hr/>
+    <h4>{_('Hojas informativas 2014')}</h4>
+    <hr/>
+</div>
+<div class="row">
+  <div class="col-xs-6 col-md-3">
+    <a href="{base_url('files/clase-2014.pdf')}" class="thumbnail">
+      <img src="{base_url('assets/img/clase-2014.png')}" alt="CLASE">
+    </a>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <a href="{base_url('files/periodica-2014.pdf')}" class="thumbnail">
+      <img src="{base_url('assets/img/periodica-2014.png')}" alt="PERIÓDICA">
+    </a>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <a href="{base_url('files/clase-periodica-2014.pdf')}" class="thumbnail">
+      <img src="{base_url('assets/img/clase-periodica-2014.png')}" alt="CLASE y PERIÓDICA">
+    </a>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <a href="{base_url('files/scielo-2014.pdf')}" class="thumbnail">
+      <img src="{base_url('assets/img/scielo-2014.png')}" alt="SciELO">
+    </a>
+  </div>
+</div>
+
+<div class="page_title">
+    <hr/>
     <h4>{_('Hojas informativas 2011')}</h4>
     <hr/>
 </div>
