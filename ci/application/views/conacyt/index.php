f3895c321368e3a26e38dee39c54e8a8067d8f59
Herz <achwazer@gmail.com>
2015-01-12 12:34:12 -0600

    _blank target o pdf files

--- ci/application/views/conacyt/index.php
+++ ci/application/views/conacyt/index.php
@@ -23,14 +23,14 @@
 					 <a data-toggle="collapse" data-parent="#collapseArea" href="#{$area.journals.0.areaConacytSlug}">
 					 	<span class="glyphicon fa fa-plus-square-o"></span> {$area.journals.0.areaConacytName}
 					 </a>
-					<a href="{base_url('archivos/conacyt/reportes/area/$area.report.2014')}"><span class="glyphicon fa fa-file-pdf-o"></span></a>
+					<a href="{base_url('archivos/conacyt/reportes/area/$area.report.2014')}" target="_blank"><span class="glyphicon fa fa-file-pdf-o"></span></a>
 			</li>
 		</ul>
 		<div class="list-group">
 			<div id="{$area.journals.0.areaConacytSlug}" class="panel-collapse collapse">
 				<div class="list-group">
 {foreach $area.journals journal}
-					<a href="{base_url('archivos/conacyt/reportes/revista/$journal.report.2014')}" class="list-group-item"><span class="glyphicon fa fa-file-pdf-o"></span> {$journal.name}</a>
+					<a href="{base_url('archivos/conacyt/reportes/revista/$journal.report.2014')}" target="_blank" class="list-group-item"><span class="glyphicon fa fa-file-pdf-o"></span> {$journal.name}</a>
 {/foreach}
 				</div>	
 			</div>
