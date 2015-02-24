ba20ff45b938779963054b62d69e8aa502627873
Herz <achwazer@gmail.com>
2015-02-23 18:15:43 -0600

    Add ppt La_importancia_de_escribir_bien_las_referencias_para_recuperar_la informacion_en_las_bases_de_datos.pptx

--- ci/application/views/main/presentaciones.php
+++ ci/application/views/main/presentaciones.php
@@ -1,12 +1,26 @@
 <div class="panel-group">
 	<div class="panel panel-default">
+		<div class="panel-heading">
+		  <h4 class="panel-title">
+			<a href="{base_url('archivos/ppt/El _futuro_de_las_revistas_de_ciencias_sociales_IIS_noviembre_2014.pptx')}" target="_blank">
+				<span class="glyphicon fa fa-file-powerpoint-o"></span> Ponencia en Mesa Redonda "El futuro de las revistas de Ciencias Sociales" 75 Aniversario de la Revista Mexicana de Sociología IIS-UNAM, 12 nov. 2014
+			</a>
+		  </h4>
+		</div>
+	</div>
+	<div class="panel panel-default">
 	    <div class="panel-heading">
 	      <h4 class="panel-title">
-	        <a href="{base_url('archivos/ppt/El _futuro_de_las_revistas_de_ciencias_sociales_IIS_noviembre_2014.pptx')}" target="_blank">
-	        	<span class="glyphicon fa fa-file-powerpoint-o"></span> Ponencia en Mesa Redonda "El futuro de las revistas de Ciencias Sociales" 75 Aniversario de la Revista Mexicana de Sociología IIS-UNAM, 12 nov. 2014
+	        <a data-toggle="collapse" data-parent="#accordion" href="#fil-xxxvi">
+	        	<span class="glyphicon fa fa-plus"></span> XXXVI Feria Internacional del Libro del Palacio de Minería, México, D.F.
 	        </a>
 	      </h4>
 	    </div>
+	    <div id="fil-xxxvi" class="panel-collapse collapse">
+	    	<ul class="list-group">
+				<li class="list-group-item"><a href="{base_url('archivos/ppt/fil-xxxvi/La_importancia_de_escribir_bien_las_referencias_para_recuperar_la informacion_en_las_bases_de_datos.pptx')}" target="_blank"><span class="glyphicon fa fa-file-powerpoint-o" ></span> Falfán Sánchez, Eugenia (2015, febrero) La importancia de escribir bien las referencias para recuperar la información en las bases de datos</a></li>
+			</ul>
+		</div>
 	</div>
 </div>
 <br/>
