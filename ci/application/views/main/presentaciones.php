535128b0dffec7c4f55b6328596212b785749b05
Herz <achwazer@gmail.com>
2015-02-23 18:16:59 -0600

    Change order in ppt presentations

--- ci/application/views/main/presentaciones.php
+++ ci/application/views/main/presentaciones.php
@@ -1,14 +1,5 @@
 <div class="panel-group">
 	<div class="panel panel-default">
-		<div class="panel-heading">
-		  <h4 class="panel-title">
-			<a href="{base_url('archivos/ppt/El _futuro_de_las_revistas_de_ciencias_sociales_IIS_noviembre_2014.pptx')}" target="_blank">
-				<span class="glyphicon fa fa-file-powerpoint-o"></span> Ponencia en Mesa Redonda "El futuro de las revistas de Ciencias Sociales" 75 Aniversario de la Revista Mexicana de Sociología IIS-UNAM, 12 nov. 2014
-			</a>
-		  </h4>
-		</div>
-	</div>
-	<div class="panel panel-default">
 	    <div class="panel-heading">
 	      <h4 class="panel-title">
 	        <a data-toggle="collapse" data-parent="#accordion" href="#fil-xxxvi">
@@ -20,6 +11,15 @@
 	    	<ul class="list-group">
 				<li class="list-group-item"><a href="{base_url('archivos/ppt/fil-xxxvi/La_importancia_de_escribir_bien_las_referencias_para_recuperar_la informacion_en_las_bases_de_datos.pptx')}" target="_blank"><span class="glyphicon fa fa-file-powerpoint-o" ></span> Falfán Sánchez, Eugenia (2015, febrero) La importancia de escribir bien las referencias para recuperar la información en las bases de datos</a></li>
 			</ul>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		  <h4 class="panel-title">
+			<a href="{base_url('archivos/ppt/El _futuro_de_las_revistas_de_ciencias_sociales_IIS_noviembre_2014.pptx')}" target="_blank">
+				<span class="glyphicon fa fa-file-powerpoint-o"></span> Ponencia en Mesa Redonda "El futuro de las revistas de Ciencias Sociales" 75 Aniversario de la Revista Mexicana de Sociología IIS-UNAM, 12 nov. 2014
+			</a>
+		  </h4>
 		</div>
 	</div>
 </div>
