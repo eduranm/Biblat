0fb1b4d3732273ef16d3b5318c2ad1767389096f
Herz <achwazer@gmail.com>
2015-02-26 22:52:20 -0600

    Add thousand separato ','

--- ci/application/views/scielo/indicadores/index_js.php
+++ ci/application/views/scielo/indicadores/index_js.php
@@ -12,7 +12,10 @@ var asyncAjax=false;
 var urlData = null;
 var lastGeneralChart = 'fasciculos';
 var cloneToolTip = {};
-Highcharts.setOptions({colors: ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099', '#0099C6', '#DD4477', '#66AA00', '#B82E2E', '#316395', '#22AA99', '#AAAA11', '#6633CC', '#E67300', '#8B0707', '#651067']});
+Highcharts.setOptions({
+	colors: ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099', '#0099C6', '#DD4477', '#66AA00', '#B82E2E', '#316395', '#22AA99', '#AAAA11', '#6633CC', '#E67300', '#8B0707', '#651067'],
+	lang: {decimalPoint: '.', thousandsSep: ','}
+});
 $(document).ready(function(){
 	$(window).bind('popstate',  function(event) {
 		console.log('pop:');
