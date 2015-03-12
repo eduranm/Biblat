55385870b958f933c101c24ccd2d1a5dd7b99028
Herz <achwazer@gmail.com>
2015-03-12 14:40:36 -0600

    Update indicator list in main

--- ci/application/views/main/index.php
+++ ci/application/views/main/index.php
@@ -10,13 +10,7 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-        <svg class="img-responsive center-block" version="1.1" id="banners_01" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 235.2 69.6" enable-background="new 0 0 235.2 69.6" xml:space="preserve" width="980" height="290">
-            <image overflow="visible" width="980" height="290" xlink:href="{base_url('img/slides/banners_01.jpg')}"  transform="matrix(0.24 0 0 0.24 0 0)"></image>
-            <text transform="matrix(4.489659e-11 -1 1 4.489659e-11 138.3335 63.3335)" fill="#FFFFFF" font-family="'MyriadPro-Regular'" font-size="3.7">{_sprintf('%s textos completos en HEVILA', number_format($totales['hevila'], 0, '.', ','))}</text>
-            <text transform="matrix(4.489659e-11 -1 1 4.489659e-11 148.8335 63.3335)" fill="#FFFFFF" font-family="'MyriadPro-Regular'" font-size="3.6">{_sprintf('%s textos completos', number_format($totales['enlaces'], 0, '.', ','))}</text>
-            <text transform="matrix(4.489659e-11 -1 1 4.489659e-11 159.557 63.3335)" fill="#FFFFFF" font-family="'MyriadPro-Regular'" font-size="3.6">{_sprintf('%s documentos', number_format($totales['documentos'], 0, '.', ','))}</text>
-            <text transform="matrix(4.489659e-11 -1 1 4.489659e-11 170.3335 63.3335)" fill="#FFFFFF" font-family="'MyriadPro-Regular'" font-size="3.6">{_sprintf('%s revistas', number_format($totales['revistas'], 0, '.', ','))}</text>
-        </svg>
+        {$svg}
     </div>
     <div class="item">
       <img class="img-responsive center-block" src="{base_url('img/slides/banners_02.jpg')}"/>
@@ -109,62 +103,31 @@
     </div><!-- Frecuencias CLAPER -->
     <div class="col-md-6">
         <h3>{_('INDICADORES BIBLIOMÉTRICOS')}</h3>
-        <ul class="orange-list list-striped">
-            <li><a href="{site_url('indicadores/indice-coautoria')}">{_('Índice de coautoría')}</a></li>
-            <li><a href="{site_url('indicadores/tasa-documentos-coautorados')}">{_('Tasa de documentos coautorados')}</a></li>
-            <li><a href="{site_url('indicadores/grado-colaboracion')}">{_('Grado de colaboración (Índice de Subramanyan)')}</a></li>
-            <li><a href="{site_url('indicadores/indice-colaboracion')}">{_('Índice de colaboración (Índice de Lawani)')}</a></li>
-            <li><a href="{site_url('indicadores/modelo-elitismo')}">{_('Modelo de Elitismo (Price)')}</a></li>
-            <li><a href="{site_url('indicadores/indice-densidad-documentos')}">{_('Índice de densidad de documentos de Zakutina y Priyenikova')}</a></li>
-            <li><a href="{site_url('indicadores/indice-concentracion')}">{_('Índice de concentración temática (Pratt)')}</a></li>
-            <li><a href="{site_url('indicadores/modelo-bradford-revista')}">{_('Modelo de Bradford por revista')}</a></li>
-            <li><a href="{site_url('indicadores/modelo-bradford-institucion')}">{_('Modelo de Bradford (Productividad institucional)')}</a></li>
-            <li><a href="{site_url('indicadores/productividad-exogena')}">{_('Productividad exógena por título de revista')}</a></li>
-            <li><a href="javascript:;">{_('Regionalización de la producción institucional')}</a></li>
-            <li><a href="javascript:;">{_('Coautoría según país de la institución de afiliación del autor')}</a></li>
-        </ul>
+        <div class="list-group list-main">
+            <a class="list-group-item"href="{site_url('indicadores/indice-coautoria')}"><span class="fa fa-line-chart"></span> {_('Índice de coautoría')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/tasa-documentos-coautorados')}"><span class="fa fa-line-chart"></span> {_('Tasa de documentos coautorados')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/grado-colaboracion')}"><span class="fa fa-line-chart"></span> {_('Grado de colaboración (Índice de Subramanyan)')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/indice-colaboracion')}"><span class="fa fa-line-chart"></span> {_('Índice de colaboración (Índice de Lawani)')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/modelo-elitismo')}"><span class="fa fa-line-chart"></span> {_('Modelo de Elitismo (Price)')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/indice-densidad-documentos')}"><span class="fa fa-line-chart"></span> {_('Índice de densidad de documentos de Zakutina y Priyenikova')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/indice-concentracion')}"><span class="fa fa-line-chart"></span> {_('Índice de concentración temática (Pratt)')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/modelo-bradford-revista')}"><span class="fa fa-line-chart"></span> {_('Modelo de Bradford por revista')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/modelo-bradford-institucion')}"><span class="fa fa-line-chart"></span> {_('Modelo de Bradford (Productividad institucional)')}</a>
+            <a class="list-group-item"href="{site_url('indicadores/productividad-exogena')}"><span class="fa fa-line-chart"></span> {_('Productividad exógena por título de revista')}</a>
+            <a class="list-group-item"href="javascript:;"><span class="fa fa-line-chart"></span> {_('Regionalización de la producción institucional')}</a>
+            <a class="list-group-item"href="javascript:;"><span class="fa fa-line-chart"></span> {_('Coautoría según país de la institución de afiliación del autor')}</a>
+        </div>
     </div><!-- Indicadores bibliometricos -->
     <div class="clearfix"></div>
     <div class="col-md-6">
         <h3>{_('INDICADORES SCIELO')}</h3>
-        <div id="carousel-scielo" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#carousel-scielo" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-scielo" data-slide-to="1"></li>
-          </ol>
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner" role="listbox">
-            <div class="item active">
-                <ul class="orange-list list-striped">
-                    <li><a href="javascript:;">{_('Indicadores bibliométricos anuales de las revistas de la red SciELO')}</a> </li>
-                    <li><a href="javascript:;">{_('Número de revistas incluidas en las colecciones de la red SciELO')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos publicados por revistas SciELO')}</a></li>
-                    <li><a href="javascript:;">{_('Número y tipo de documento publicados en las revistas de las colecciones SciELO')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos incluidos en las colecciones SciELO por área del conocimiento')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos incluidos por área del conocimiento en las colecciones SciELO')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos incluidos en la red SciELO según país de la afiliación del autor')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos publicados por revistas SciELO según el país de la afiliación del autor')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos publicados por revistas SciELO según el país de publicación de la revista y país de la afiliación del autor')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos publicados por revistas SciELO según área del conocimiento y país de la afiliación del autor')}</a></li>
-                </ul>
-            </div>
-            <div class="item">
-                <ul class="orange-list list-striped">
-                    <li><a href="javascript:;">{_('Número de citas recibidas por revista SciELO según área del conocimiento de la revista citante')}</a></li>
-                    <li><a href="javascript:;">{_('Número de citas recibidas por revista SciELO según país de la afiliación del autor citante')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos citados según la edad de citación en artículos incluidos en todas las colecciones SciELO')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos citados según la edad de citación por título de revista')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos citados según la edad de citación por área del conocimiento de las revistas citantes')}</a></li>
-                    <li><a href="javascript:;">{_('Número de artículos citados según la edad de citación por país de la afiliación del autor citante')}</a></li>
-                    <li><a href="javascript:;">{_('Tipo de documento citado por los artículos incluidos en todas las colecciones SciELO')}</a></li>
-                    <li><a href="javascript:;">{_('Tipo de documento citado por revista citante')}</a></li>
-                    <li><a href="javascript:;">{_('Tipo de documento citado por área del conocimiento de la revista citante')}</a></li>
-                    <li><a href="javascript:;">{_('Tipo de documento citado por país de la afiliación del autor citante')}</a> </li>
-                </ul>
-            </div>
-          </div>
-        </div><!-- carousel-scielo -->
+        <div class="list-group list-main">
+            <a class="list-group-item"href="{site_url('scielo/indicadores/distribucion-articulos-coleccion')}"><span class="fa fa-line-chart"></span> {_('Distribución de artículos por colección')}</a>
+            <a class="list-group-item"href="{site_url('scielo/indicadores/distribucion-revista-coleccion')}"><span class="fa fa-line-chart"></span> {_('Distribución de revistas por colección')}</a>
+            <a class="list-group-item"href="{site_url('scielo/indicadores/indicadores-generales-revista')}"><span class="fa fa-line-chart"></span> {_('Indicadores generales por revistas')}</a>
+            <a class="list-group-item"href="{site_url('scielo/indicadores/citacion-articulos-edad')}"><span class="fa fa-line-chart"></span> {_('Distribución de artículos por edad del documento citado')}</a>
+            <a class="list-group-item"href="{site_url('scielo/indicadores/citacion-articulos-tipo')}"><span class="fa fa-line-chart"></span> {_('Distribución de artículos por tipo del documento citado')}</a>
+        </div>
     </div><!-- Indicadore SciELO -->
     <div class="col-md-6">
         <h3>{_('OTROS INDICADORES')}</h3>
