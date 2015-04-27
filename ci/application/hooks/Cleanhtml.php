39151b10003cd6f2362109c9f60c51a493fe5e40
Herz <achwazer@gmail.com>
2015-04-27 14:37:22 -0500

    Minify HTML content

--- ci/application/hooks/Cleanhtml.php
+++ ci/application/hooks/Cleanhtml.php
@@ -7,15 +7,21 @@ class Cleanhtml {
 	*/
 	function output()
 	{
+		ini_set("pcre.recursion_limit", "16777");
 		$CI =& get_instance();
 		$buffer = $CI->output->get_output();
 		/*Remove debug console log in production*/
 		if(ENVIRONMENT === "production"):
 			$buffer = preg_replace('/console.log\(.+?\);/', "", $buffer);
 			$buffer = preg_replace('/<!--.+?-->/', "", $buffer);
+			$re = '%(?>[^\S ]\s*|\s{2,})(?=[^<]*+(?:<(?!/?(?:textarea|pre|script)\b)[^<]*+)*+(?:<(?>textarea|pre|script)\b|\z))%Six';
+			$new_buffer = preg_replace($re, " ", $buffer);
+			if ($new_buffer === null)
+				$new_buffer = $buffer;
+			$CI->output->set_output($new_buffer);
+		else:
+			$CI->output->set_output($buffer);
 		endif;
-		
-		$CI->output->set_output($buffer);
 		$CI->output->_display();
 	}
 }
