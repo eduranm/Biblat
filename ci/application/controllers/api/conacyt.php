70b2a057357ba22b52e3adbbf21a624beb9c0cab
Herz <achwazer@gmail.com>
2015-05-11 15:59:38 -0500

    List all journals when no issn request

--- ci/application/controllers/api/conacyt.php
+++ ci/application/controllers/api/conacyt.php
@@ -11,55 +11,56 @@ class Conacyt extends REST_Controller {
 	public function indicadores_get(){
 		$data = array();
 		$issn = $this->get('issn') ? $this->get('issn') : NULL;
-		if($issn == NULL):
-			$data['response'] = 'error';
-			$data['message'] = 'ISSN is needed to request, example: '.site_url('api/indicadores/issn/0036-3634');
-			$this->response($data, 500);
-		else:
-			$data['indicators'] = array();
-			$this->load->database();
-			$scieloDB = $this->load->database('scielo', TRUE);
-			$query = "SELECT * FROM \"vJournalConacyt\" WHERE issn='{$issn}' LIMIT 1";
-			$query = $scieloDB->query($query);
-			$journal = $query->row_array();
-			$scieloDB->close();
-			$data['journal'] = $journal['name'];
+		$this->load->database();
+		$scieloDB = $this->load->database('scielo', TRUE);
+		$query = "SELECT * FROM \"vJournalConacyt\"";
+		if(isset($issn))
+			$query = "{$query} WHERE issn='{$issn}' LIMIT 1";
+		$query = $scieloDB->query($query);
+		$scieloDB->close();
+		foreach ($query->result_array() as $journal):
+			$data[$journal['issn']]['indicators'] = array();
+			$data[$journal['issn']]['name'] = $journal['name'];
 			$report['2014'] = strtoupper($journal['neumonic'])."_Reporte_bibliometrico_DIC2014.pdf";
 			if(file_exists("archivos/conacyt/reportes/revista/{$row['report']['2014']}")):
-				$data['indicators'][] = array(
+				$data[$journal['issn']]['indicators'][] = array(
 						'title' => 'Reporte bibliométrico CONACYT',
 						'url' => site_url("conacyt/revista/{$journal['slug']}"),
 						'img' => null
 					);
 			endif;
+		endforeach;
 
-			$query = "SELECT * FROM \"vIndicadoresRevistaGeneral\" WHERE \"revistaISSN\"='{$issn}' LIMIT 1";
-			$query = $this->db->query($query);
-			$journal = $query->row_array();
-			$this->db->close();
-			$data['journal'] = $journal['revista'];
+		$query = "SELECT * FROM \"vIndicadoresRevistaGeneralConacyt\"";
+		if(isset($issn))
+			$query = "{$query} WHERE \"revistaISSN\"='{$issn}' LIMIT 1";
+		$query = $this->db->query($query);
+		$this->db->close();
+		foreach ($query->result_array() as $journal):
+			$data[$journal['revistaISSN']]['name'] = $journal['revista'];
 			if($journal['pratt']):
-				$data['indicators'][] = array(
+				$data[$journal['revistaISSN']]['indicators'][] = array(
 						'title' => 'Índice de concentración Pratt (Biblat)',
 						'url' => site_url("indicadores/indice-concentracion/disciplina/{$journal['disciplinaSlug']}/revista/{$journal['revistaSlug']}"),
 						'img' => null
 					);
 			endif;
 			if($journal['exogena']):
-				$data['indicators'][] = array(
+				$data[$journal['revistaISSN']]['indicators'][] = array(
 						'title' => 'Tasa de autoría exógena (Biblat)',
 						'url' => site_url("indicadores/productividad-exogena/disciplina/{$journal['disciplinaSlug']}/revista/{$journal['revistaSlug']}"),
 						'img' => null
 					);
 			endif;
 			if($journal['generalesrevista']):
-				$data['indicators'][] = array(
+				$data[$journal['revistaISSN']]['indicators'][] = array(
 						'title' => 'Indicadores generales por revista (SciELO)',
 						'url' => site_url("scielo/indicadores/indicadores-generales-revista/coleccion/{$journal['networkSlug']}/revista/acta-botanica-mexicana{$journal['revistaSlug']}"),
 						'img' => null
 					);
 			endif;
-			$this->response($data, 200);
-		endif;
+		endforeach;
+		
+		$this->response($data, 200);
 	}
 }
