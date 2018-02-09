17375a6bf02c85caaaf5a697b7060c389860c31b
Arturo Rendón Cruz <achwazer@gmail.com>
2018-02-09 10:00:38 -0600

    Add missing semicolon

--- ci/application/views/javascript/biblat.php
+++ ci/application/views/javascript/biblat.php
@@ -160,7 +160,7 @@ $(document).on('click', '.translate', function(e) {
 			dataType: 'json',
 			data: $(this).serialize(),
 			success: function(data) {
-				console.log(data)
+				console.log(data);
 				loading.end();
 				if(data.type == 'success'){
 					$('input:text').each(function(){
@@ -200,26 +200,26 @@ $(window).bind('beforeunload', function() {
 var backdrop = '.dropdown-backdrop';
 var toggle = '[data-toggle=dropdown]';
 var Dropdown = function (element) {
-$(element).on('click.bs.dropdown', this.toggle)
+$(element).on('click.bs.dropdown', this.toggle);
 };
 Dropdown.prototype.toggle = function (e) {
 var $this = $(this);
 if ($this.is('.disabled, :disabled')) return;
 var $parent = getParent($this);
 var isActive = $parent.hasClass('open');
-clearMenus($(this))
+clearMenus($(this));
 if (!isActive) {
 if ('ontouchstart' in document.documentElement && !$parent.closest('.navbar-nav').length) {
 // if mobile we use a backdrop because click events don't delegate
-$('<div class="dropdown-backdrop"/>').insertAfter($(this)).on('click', clearMenus)
+$('<div class="dropdown-backdrop"/>').insertAfter($(this)).on('click', clearMenus);
 }
-var relatedTarget = { relatedTarget: this }
-$parent.trigger(e = $.Event('show.bs.dropdown', relatedTarget))
+var relatedTarget = { relatedTarget: this };
+$parent.trigger(e = $.Event('show.bs.dropdown', relatedTarget));
 if (e.isDefaultPrevented()) return;
 $parent
 .toggleClass('open')
-.trigger('shown.bs.dropdown', relatedTarget)
-$this.focus()
+.trigger('shown.bs.dropdown', relatedTarget);
+$this.focus();
 }
 return false;
 }
@@ -232,7 +232,7 @@ if ($this.is('.disabled, :disabled')) return;
 var $parent = getParent($this);
 var isActive = $parent.hasClass('open');
 if (!isActive || (isActive && e.keyCode == 27)) {
-if (e.which == 27) $parent.find(toggle).focus()
+if (e.which == 27) $parent.find(toggle).focus();
 return $this.click();
 }
 var desc = ' li:not(.divider):visible a';
@@ -258,19 +258,19 @@ if ($DoNotCloseThisOne) return;
 }
 var relatedTarget = { relatedTarget: this };
 if (!$parent.hasClass('open')) return;
-$parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget))
+$parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget));
 if (e.isDefaultPrevented()) return;
-$parent.removeClass('open').trigger('hidden.bs.dropdown', relatedTarget)
+$parent.removeClass('open').trigger('hidden.bs.dropdown', relatedTarget);
 })
 }
 function getParent($this) {
-var selector = $this.attr('data-target')
+var selector = $this.attr('data-target');
 if (!selector) {
-selector = $this.attr('href')
+selector = $this.attr('href');
 selector = selector && /#[A-Za-z]/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
 }
 var $parent = selector && $(selector);
-return $parent && $parent.length ? $parent : $this.parent()
+return $parent && $parent.length ? $parent : $this.parent();
 }
 // DROPDOWN PLUGIN DEFINITION
 // ==========================
@@ -279,8 +279,8 @@ $.fn.dropdown = function (option) {
 return this.each(function () {
 var $this = $(this);
 var data = $this.data('bs.dropdown');
-if (!data) $this.data('bs.dropdown', (data = new Dropdown(this)))
-if (typeof option == 'string') data[option].call($this)
+if (!data) $this.data('bs.dropdown', (data = new Dropdown(this)));
+if (typeof option == 'string') data[option].call($this);
 })
 }
 $.fn.dropdown.Constructor = Dropdown;
