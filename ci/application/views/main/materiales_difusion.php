f34a5d6e5f8c79a947e4896efc162b7f0ab73166
arendon <arendon@iMac-de-Arturo.local>
2018-04-19 12:41:34 -0500

    Update materials

--- ci/application/views/main/materiales_difusion.php
+++ ci/application/views/main/materiales_difusion.php
@@ -1,5 +1,28 @@
 <div class="page_title">
     <hr/>
+    <h4>{_('Hojas informativas 2018')}</h4>
+    <hr/>
+</div>
+<div class="row">
+  <div class="col-xs-6 col-md-3">
+    <a href="{base_url('archivos/clase-2018.pdf')}" class="thumbnail">
+      <img src="{base_url('assets/img/clase-2018.png')}" alt="CLASE">
+    </a>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <a href="{base_url('archivos/periodica-2018.pdf')}" class="thumbnail">
+      <img src="{base_url('assets/img/periodica-2018.png')}" alt="PERIÓDICA">
+    </a>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <a href="{base_url('archivos/clase-periodica-2018.pdf')}" class="thumbnail">
+      <img src="{base_url('assets/img/clase-periodica-2018.png')}" alt="CLASE y PERIÓDICA">
+    </a>
+  </div>
+</div>
+
+<div class="page_title">
+    <hr/>
     <h4>{_('Hojas informativas 2014')}</h4>
     <hr/>
 </div>
