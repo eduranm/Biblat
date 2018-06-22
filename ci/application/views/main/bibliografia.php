b30d0f1ce63351545086f2e399f33f81fc85e04c
arendon <arendon@iMac-de-Arturo.local>
2018-06-22 14:09:04 -0500

    Add file to bibliography

--- ci/application/views/main/bibliografia.php
+++ ci/application/views/main/bibliografia.php
@@ -1,4 +1,6 @@
 <table class="table table-striped">
+	<tr><td><cite>Rosas Rivera, Paola Vanesa (2018). <a href="{base_url('archivos/pdf/CLASE_Y_PERIODICA_tesis_de_licenciatura_Paola_Vanesa_Rosas_2018.pdf')}" target="blank">CLASE y PERIÓDICA: Análisis y propuesta de ampliación en la organización del conocimiento en las tablas de disciplinas para la indización de documentos (Tesis de Licenciatura)</a>. México, Ciudad de México, Universidad Nacional Autónoma de México, Facultad de Filosofía y Letras, Colegio de Bibliotecología</cite></td></tr>
+
 	<tr><td><cite>Alonso Gamboa, J. O. & Reyna Espinosa, F. R. (2015). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/AlonsoGamboa_ReynaEspinosa_2015.pdf" target="blank">Revistas académicas mexicanas. Panorama y prospectiva</a>. Ciencia ergo sum, 22(3), 181-191.</cite></td></tr>
 
 	<tr><td><cite>Reyna Espinosa, F. R. (2015). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/ReynaEspinosa_2015%282%29.pdf" target="_blank">CLASE. Perfil de una base de datos bibliográfica</a>. Biblioteca Universitaria 18 (2), 144-153.</cite></td></tr>
@@ -8,23 +10,23 @@
 	<tr><td><cite>Rodríguez Yunta, L.  (2014, abril) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/RodriguezYunta_2014.pdf" target="_blank">Indicadores bibliométricos sobre revistas: más allá de los índices de citas.</a> Ponencia presentada en el XI Seminario Hispano-Mexicano de Investigación en Bibliotecología y Documentación: La información y sus contextos en el cambio social.  México: UNAM, Instituto de Investigaciones Bibliotecológicas y de la Información.</cite></td></tr>
 
 	<tr><td><cite>Reyna Espinosa, F. R. (2013) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/ReynaEspinosa_2013.pdf" target="_blank">Reseña de libros</a> [Reseña del Manual de indización para las bases de datos clase y periódica]. Biblioteca Universitaria 15(2), 201-202.</cite></td></tr>
-	
+
 	<tr><td><cite>Salinas Ibarra, Hugo (2013). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Salinas_Ibarra_2013.pdf" target="_blank">Análisis Sociológico del proceso de producción y difusión y consumo de información científica. El caso de la base de datos CLASE</a>. Informe de servicio social (Licenciatura). México, Distrito Federal, Universidad Nacional Autónoma de México, Facultad de Ciencias Políticas y Sociales.</cite></td></tr>
 
 	<tr><td><cite>Sánchez Pereyra, A., Quintero León, K., García Bravo, I. & Rendón Cruz, A. (2013, abril)  <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/SanchezPereyra_Quintero_Garcia_Rendon_2013.pdf" target="blank">BIBLAT: Indicadores bibliométricos alternativos de la producción científica publicada en revistas de América Latina y el Caribe.</a> Ponencia presentada en el  Taller sobre Indicadores en Ciencia y Tecnología en Latinoamérica (TICyTLA). México: UNAM; Foro Consultivo Científico y Tecnológico (FCCyT); ITAM; Cinvestav.</cite></td></tr>
-	
+
 	<tr><td><cite>Alonso Gamboa, J. O., Arana Mendoza, C., Reyna Espinosa, F. R &amp; Sánchez Pereyra, A. (2012)<a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Manual_Clase_Periodica_2012.pdf" target="_blank"> Manual de indización para las bases de datos clase y periódica</a>. DGB-UNAM, México.</cite></td></tr>
 
 	<tr><td><cite>Alonso Gamboa, J. O. &amp; Russell, J. M. (2012) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/AlonsoGamboa_2012.pdf" target="_blank">Latin American scholarly journal databases: a look back to the way forward.</a> Aslib Proceedings. 64(1), 32-45.</cite></td></tr>
-			
+
 	<tr><td><cite>Sánchez Pereyra, A., Cetto, A.M., Takayanagui, R. &amp; Alonso-Gamboa J.O. (2012) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Sanchez_Cetto_ColoquioCLACSO_2012.pdf" target="_blank">Iniciativas UNAM para acceso abierto a las Ciencias Sociales de América Latina y el Caribe: Latindex, SciELO, CLASE.</a>[Diapositivas] Coloquio El conocimiento como bien común: acceso abierto a las Ciencias Sociales de América Latina y el Caribe, Conferencia Latinoamericana y Caribeña de Ciencias Sociales de CLACSO. México, D.F.</cite></td></tr>
-	
+
 	<tr><td><cite>Michán, L. (2011). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Michan_2011.pdf" target="_blank">Análisis bibliométrico de la producción sistemática en América Latina.</a> Acta Biológica Colombiana, 16(2), 33–46.</cite></td></tr>
 
 	<tr><td><cite>Rodríguez León, R. (2011) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/RodriguezLeon_2011.pdf" target="_blank">Fueron expuestos en la XXXII FILPM los indicadores bibliométricos BIBLAT, ECUM y SciELO.</a> Biblioteca Universitaria, 14(1), 101–105.</cite></td></tr>
 
 	<tr><td><cite>Rodríguez Yunta, L. &amp;  Alonso Gamboa, J. O. (2011, junio-julio) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/RodriguezYunta_AlonsoGamboa_2011.pdf" target="_blank">Normalización del vocabulario de indización en bases de datos multidisciplinares de ciencias sociales y humanas: análisis comparativo de la experiencia de ISOC (España) y CLASE (México) desde la década de 1970 hasta 2010.</a> Ponencia presentada en el X Congreso ISKO.  El Ferrol, España.</cite></td></tr>
-	
+
 	<tr><td><cite>Sánchez Pereyra, A. (2011) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/SanchezPereyra_Biblat_2011.pdf" target="_blank">Biblat: Indicadores bibliométricos para las revistas académicas latinoamericanas.</a> [Diapositivas] XLII Jornadas Mexicanas de Biblioteconomía "Bibliotecas y liderazgo en tiempos de transición". San Luis Potosí, S.L.P., México.</cite></td></tr>
 
 	<tr><td><cite>Sánchez Pereyra, A. (2011) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/SanchezPereyra_Portal_2011.pdf" target="_blank">Portal de Indicadores Bibliométricos Biblat. Bases de datos Clase y Periódica.</a> [Diapositivas] XXXII Feria Internacional del Libro, México, D.F.</cite></td></tr>
@@ -34,9 +36,9 @@
 	<tr><td><cite>Sánchez Pereyra, A. (2010, septiembre). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/SanchezPereyra_Ponencia_2010.pdf" target="_blank">Latin American Scientific Journals from "Lost Science" to Open Access"</a>, ponencia presentada en el Panel Ciencia: Acceso Abierto y Visibilidad de la Ciencia del Encuentro de Mexicanistas 2010, Educación, Ciencia y Cultura. Amberes, Bélgica</cite></td></tr>
 
 	<tr><td><cite>Pire Cordero, R. (2010) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/PireCordero_2010.pdf" target="_blank">Disponibilidad de bases de datos oficiales como soporte a la investigación</a>. COMPENDIUM 25, 65-63.</cite></td></tr>
-			
+
 	<tr><td><cite>Reséndiz Cansino, G., Sánchez Pereyra, A., Sánchez, M. &amp; García Bravo, I. (2010). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/ResendizCansino_2010.pdf" target="_blank">Portal de indicadores bibliométricos BIBLAT.</a> Biblioteca Universitaria 13(2), 175–188.</cite></td></tr>
-	
+
 	<tr><td><cite>Reyna Espinosa, F. R. (2010). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/ReynaEspinosa_2010.pdf" target="_blank">Trascendencia de la Bibliografía Latinoamericana de la UNAM</a>, Biblioteca Universitaria 13(2), 164–174.</cite></td></tr>
 
 	<tr><td><cite>Rodríguez Gallardo, A. (2010, agosto). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/RodriguezGallardo_2010.pdf" target="_blank">Visibilidad de la ciencia Latinoamericana: un reto para la bibliotecología y la información en la región por más de 35 años.</a> Ponencia presentada en el World Library and Information Congress: 76th IFLA General Conference and Assembly, Gothenburg, Sweden</cite></td></tr>
@@ -44,19 +46,19 @@
 	<tr><td><cite>Rodríguez Yunta, L. (2010). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/RodriguezYunta_2010.pdf" target="_blank">Ciencia y Documentación en español en el espacio virtual iberoamericano. Rol de las revistas científicas y desarrollo de nuevos productos electrónicos de información bibliográfica.</a> Tesis (Doctorado). Madrid, España. Universidad Complutense de Madrid, Facultad de Ciencias de la Documentación.</cite></td></tr>
 
 	<tr><td><cite><a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Libera_la_UNAM_Biblat.pdf" target="_blank">Libera la UNAM el portal Biblat, valiosa fuente bibliográfica para conocer la productividad científica de América Latina y el Caribe.</a> (2009). Humanidades y Ciencias Sociales, 44, 17.</cite></td></tr>
-			
+
 	<tr><td><cite>Guzmán Sánchez, M. V. (2009). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/GuzmanSanchez_2009.pdf" target="_blank">La Dirección General de Bibliotecas de la Universidad Nacional Autónoma de México libera el portal Biblat</a>. ACIMED, 20(3), 101–102.</cite></td></tr>
-	
+
 	<tr><td><cite>Michán, L., Russell, J. M., Sánchez Pereyra, A., Llorens Cruset, A. &amp; López Beltrán, C. (2008). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Michan_Russell_Sanchez_2008.pdf" target="_blank">Análisis de la sistemática actual en Latinoamérica.</a> Interciencia, 33(10), 754–761.</cite></td></tr>
 
 	<tr><td><cite>Russell, J. M., Madera-Jaramillo, M. J., Hernández-García, Y. &amp; Ainsworth, S. (2008, julio-agosto). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Russell_2008.pdf" target="_blank">Mexican collaboration networks in the international and regional arenas.</a> In Fourth International Conference on Webometrics, Informetrics and Scientometrics &amp; Ninth COLLNET Meeting. Berlin: Humboldt-Universität zu Berlin, Institute for Library and Information Science (IBI).</cite></td></tr>
-			
+
 	<tr><td><cite>Alonso Gamboa, J. O. &amp; Reyna Espinosa, F. R. (2005). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/AlonsoGamboa_ReynaEspinosa_2005.pdf" target="_blank">Compilación de datos bibliométricos regionales usando las bases de datos clase y periódica.</a> Revista Interamericana de Bibliotecología 28(1), 63–78.</cite></td></tr>
-			
+
 	<tr><td><cite>López Ornelas, M. (2004). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/LopezOrnelas_2004.pdf" target="_blank">Diseño y validación de un instrumento para evaluar revistas académicas electrónicas en internet.</a> Tesis (Maestría). Ensenada, Baja California. Universidad Autónoma de Baja California, Instituto de Investigación y Desarrollo Educativo</cite></td></tr>
-			
+
 	<tr><td><cite>Alonso Gamboa, J. O. (2003). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/AlonsoGamboa_2003.pdf" target="_blank">Selección de revistas latinoamericanas en bases de datos: criterios utilizados en Clase y Periódica.</a> Biblioteca Universitaria, 6(1), 9–21.</cite></td></tr>
-	
+
 	<tr><td><cite>Aguilar Rocha, B. E. &amp; Lázaro-Chávez Mancilla, E. (2000, noviembre). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/AguilarRocha_LazaroChavez_2000.pdf" target="_blank">CLASE: fuente de información especializada. Análisis sobre el tema investigación educativa.</a> Ponencia presentada en la 8a Reunión sobre Revistas Académicas y de Investigación. Las revistas científicas latinoamericanas su difusión y acceso a través de bases de datos. México: UNAM, Dirección General de Bibliotecas.</cite></td></tr>
 
 	<tr><td><cite>Alonso Gamboa, J. O. (2000, noviembre) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/AlonsoGamboa_2000.pdf" target="_blank">Iniciativas para aumentar la visibilidad de las revistas latinoamericanas. La aportación de Clase, Periódica y Latindex.</a> Ponencia presentada en la 8a Reunión sobre Revistas Académicas y de Investigación. Las revistas científicas latinoamericanas su difusión y acceso a través de bases de datos. México: UNAM, Dirección General de Bibliotecas.</cite></td></tr>
@@ -64,21 +66,21 @@
 	<tr><td><cite>Montes Flores, M. A. (2000, noviembre). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/MontesFlores_2000.pdf" target="_blank">Recuperación de información en CLASE: Un enfoque basado en el concepto de metadatos.</a> Ponencia presentada en la 8a Reunión sobre Revistas Académicas y de Investigación. Las revistas científicas latinoamericanas su difusión y acceso a través de bases de datos. México: UNAM, Dirección General de Bibliotecas.</cite></td></tr>
 
 	<tr><td><cite>Reyna Espinosa, F. R. (2000, noviembre) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/ReynaEspinosa_2000.pdf" target="_blank">Clase a 25 años de existencia.</a> Ponencia presentada en la 8a Reunión sobre Revistas Académicas y de Investigación. Las revistas científicas latinoamericanas su difusión y acceso a través de bases de datos. México: UNAM, Dirección General de Bibliotecas.</cite></td></tr>
-			
+
 	<tr><td><cite>Sánchez Pereyra, A. (2000, noviembre) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/SanchezPereyra_2000.pdf" target="_blank">La base de datos CLASE como recurso de información para el análisis económico latinoamericano en la "era de la globalización"	.</a> Ponencia presentada en la 8a Reunión sobre Revistas Académicas y de Investigación. Las revistas científicas latinoamericanas su difusión y acceso a través de bases de datos. México: UNAM, Dirección General de Bibliotecas.</cite></td></tr>
-					
+
 	<tr><td><cite>Alonso Gamboa, J. O. (1998). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/AlonsoGamboa_1998.pdf" target="_blank">Acceso a revistas latinoamericanas en Internet. Una opción a través de las bases de datos Clase y Periódica.</a> Ciência da Informação, 27(1), 90–95.</cite></td></tr>
-			
+
 	<tr><td><cite>Cetto, A. M. &amp; Alonso Gamboa, J. O. (1998) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Cetto_AlonsoGamboa_1998.pdf" target="_blank">Scientific periodicals in Latin America and the Caribbean: a perspective</a>. Interciencia, 23(2), 84-93.</cite></td></tr>
-			
+
 	<tr><td><cite>Reyna Espinosa, F. R. &amp; Alonso Gamboa, J. O. (1997, octubre). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/ReynaEspinosa_AlonsoGamboa_1997.pdf" target="_blank">Retos y prospectiva de las bases de datos latinoamericanas: la experiencia de BLAT, CLASE y PERIODICA.</a> Ponencia presentada en el Congreso Internacional de Informacion INFO 97. Habana, Cuba: Instituto de Información Científica y Tecnológica.</cite></td></tr>
 
 	<tr><td><cite>Dirección General de Planeación. (1996). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/MemoriaUNAM_1996.pdf" target="_blank">Centro de Información Científica y Humanística. En Memoria UNAM 1996.</a> Universidad Nacional Autónoma de México.</cite></td></tr>
-			
+
 	<tr><td><cite>Alonso Gamboa, J. O. (1993). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/AlonsoGamboa_1993.pdf" target="_blank">Servicios, productos, docencia e investigación en información. La experiencia del Centro de Información Científica y Humanística de la Universidad Nacional Autónoma de México.</a> Ciencias de la Información24(4), 201-209.</b></cite></td></tr>
 
 	<tr><td><cite>Reyna Espinosa, F. R. (1992) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/ReynaEspinosa_1992.pdf" target="_blank">Algunos aspectos referentes al análisis de información en el Centro de Información Científica y Humanística de la UNAM.</a> Ponencia presentada en el Primer Foro de Información: bases de datos y perspectivas  LatinBase 92. Guadalajara, México.</cite></td></tr>
-			
+
 	<tr><td><cite>Guerra, R. (1992) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Guerra_1992.pdf" target="_blank">La bibliografía Latinoamericana en el CICH. Balance de 20 años.</a> Ponencia presentada en el Primer Foro de Información: bases de datos y perspectivas LatinBase 92, Guadalajara, México.</cite></td></tr>
 
 	<tr><td><cite>Buttenklepper, A.; Pérez, A.; Sandoval, A. &amp; Almada de Ascencio, M. (1983). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Buttenklepper_Perez_Sandoval_Almada_1983.pdf" target="_blank">Research at Latin American institutions of higher education. A bibliometric approach</a>. Inforum, 3, 13-23.</cite></td></tr>
@@ -90,16 +92,16 @@
 	<tr><td><cite>Sandoval, A.; Pérez, A. &amp; Franco, C. (1983). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Sandoval_Perez_Franco_1983.pdf" target="_blank">A current Latin American Bibliography compiled from non-Latin American journals. A Bibliometric study</a>. Inforum, 3, 33-39.</cite></td></tr>
 
 	<tr><td><cite>Garfield, E (1983) <a href="http://132.248.9.34/hevila/Inforum/1983/no2/2.pdf" target="_blank">Las universidades como productoras y consumidoras de información</a> &lt;Conferencia presentada el 24 de septiembre de 1976 en la UNAM&gt;. Inforum, 2, 1-15</cite></td></tr>
-	
+
 	<tr><td><cite>Maffey, L., Arana Mendoza, C., Deschamps, I. &amp; Sosa, V. (1982). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Maffey_Arana_Deschamps_1982.pdf" target="_blank">Periódica : un índice de revistas latinoamericanas en ciencia y tecnología.</a> Revista Española de Documentación Cientifíca, 6(2), 137–156.</cite></td></tr>
-			
+
 	<tr><td><cite>Pérez-Vitoria, A. (1982). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Perez-Vitoria_1982_1.pdf" target="_blank">La información en el marco de la cooperación científica internacional</a>. Inforum, 1, 1-31.</cite></td></tr>
-	
+
 	<tr><td><cite>Pérez-Vitoria, A. (1982) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Perez-Vitoria_1982.pdf" target="_blank">Los primeros diez años del CICH-UNAM</a>. RUCIBA, 4(3), 195-206</cite></td></tr>
 
 	<tr><td><cite>Buttenkleppler, A. (1978?). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/B%fcttenkleppler_1978.pdf" target="_blank">Centro de Información Científica y Humanística. Vol. 5.</a>. México : UNAM, CICH, p. 439-519</cite></td></tr>
-			
+
 	<tr><td><cite>Sandoval, A. (1978). <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Sandoval_1978.pdf" target="_blank">Centro de información Científica y Humanística: un centro de información universitario del tercer mundo</a>. Boletín de la Unesco para las Bibliotecas, 32(1), 45-52</cite></td></tr>
-			
+
 	<tr><td><cite>Sandoval, A. (1956, diciembre) <a href="http://132.248.9.34/hevila/e-BIBLAT/Biblio/Sandoval_1956.pdf" target="_blank">El centro de Documentación científica y técnica de México y la bibliografía científica en América Latina</a>. Ponencia presentada en las Primeras Jornadas de Biblioteconomía, Bibliografía y Canje de la AMBAC. México</cite></td></tr>
 </table>
