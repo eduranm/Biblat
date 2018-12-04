4eb3a3c15c8c7598ba251f80b1f7e51bea942c77
Arturo Rendón <arendon@dgb.unam.mx>
2018-12-04 15:58:35 -0600

    Add PERIODICA 40 Anniversary

--- ci/application/views/main/presentaciones.php
+++ ci/application/views/main/presentaciones.php
@@ -2,6 +2,14 @@
 	<div class="panel panel-default">
 		<div class="panel-heading">
 			<h5 class="panel-title">
+					<a href="{base_url('archivos/pdf/Aniversario_40_PERIODICA_nov_2018.pdf')}" target="_blank"><span class="fa fa-file-pdf-o"></span></a> PERIODICA - 40 Aniversario
+
+			</h5>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h5 class="panel-title">
 					<a href="{base_url('archivos/pdf/Poster_BIBLAT_CRECS_2018.pdf')}" target="_blank"><span class="fa fa-file-pdf-o"></span></a> <a href="{base_url('archivos/img/Poster_BIBLAT_CRECS_2018.jpg')}" target="_blank"><span class="fa fa-file-image-o"></span></a> BIBLAT Indicadores bibliométricos para la ciencia de América Latina, poster ganador de la modalidad de diseño en la 8ª Conferencia Internacional sobre Revistas Científicas CRECS 2018, Barranquilla, Colombia, 2-4 mayo de 2018
 
 			</h5>
