33a8992a1d36177b4ba719b3f46dbc7a3ec76569
Edgar <eduranm@dgb.unam.mx>
2020-10-12 19:01:04 -0500

    Actualización en presentaciónes y multimedia

--- ci/application/views/main/multimedia.php
+++ ci/application/views/main/multimedia.php
@@ -4,6 +4,31 @@
 	      <h5 class="panel-title">
                     <div class="row">
                         <div class="col-xs-12 col-sm-6 col-md-4">
+                            <iframe width="250" src="https://www.youtube.com/embed/WgsELl6Pj7A" allowfullscreen></iframe>
+                            <div>&nbsp;</div>
+                        </div>
+           
+                        <div class="col-xs-12 col-sm-6 col-md-8">
+                            <a href="https://www.youtube.com/embed/WgsELl6Pj7A" target="_blank">
+                                <span class="fa fa-youtube-square"></span> 
+                                BIBLAT presenta nuevo módulo para que los editores autoevalúen sus revistas con los criterios de evaluación de las bases de datos y sometan su candidatura.
+                            </a>    
+                            <br><br>IV Jornada LatinREV - Panel II Indización, redes sociales y buscadores bibliográficos académicos: estrategias de las revistas - Exposiciones de H. Degiorgis, C. Authier, A. Sánchez Pereyra y M. A. Chávez Flores - 8/10/2020
+                            <br><br>Introducción de Antonio Sánchez, en minuto 42:51
+                            <br>Presentación del módulo de Manuel Alejandro Flores, en minuto 52:10
+                            
+                        </div>
+                    </div>
+	      </h5>
+	    </div>
+	</div>
+</div>
+<div class="panel-group">
+	<div class="panel panel-default">
+	    <div class="panel-heading">
+	      <h5 class="panel-title">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-4">
                             <iframe width="250" src="https://www.youtube.com/embed/h571rion4vI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             <div>&nbsp;</div>
                         </div>
